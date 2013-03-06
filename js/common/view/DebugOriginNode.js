c9c99ae43d90a5c31434e37268370901546cd6da
Chris Malley <cmalley@pixelzoom.com>
2013-03-05 19:52:39 -0700

    doc

--- js/common/view/DebugOriginNode.js
+++ js/common/view/DebugOriginNode.js
@@ -1,7 +1,8 @@
 // Copyright 2002-2013, University of Colorado
 
 /**
- * To debug the origin of a composite node, add an instance of this node as a child.
+ * To debug a node's origin, add an instance of this node as a child.
+ * Be careful not to change this node's position, leave it at (0,0).
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
