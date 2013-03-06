f62befb8541ac881545d6cc602360e693a143f48
Chris Malley <cmalley@pixelzoom.com>
2013-03-06 13:12:38 -0700

    initial layout of faucet images

--- js/concentration/view/FaucetNode.js
+++ js/concentration/view/FaucetNode.js
@@ -12,12 +12,19 @@ define(
     "SCENERY/nodes/Node",
     "SCENERY/nodes/Image",
     "PHETCOMMON/util/Inheritance",
+    "common/view/DebugOriginNode",
     "image!images/faucet_handle.png",
     "image!images/faucet_pipe.png",
     "image!images/faucet_pivot.png",
     "image!images/faucet_spout.png"
   ],
-  function ( Node, Image, Inheritance, handleImage, pipeImage, pivotImage, spoutImage ) {
+  function ( Node, Image, Inheritance, DebugOriginNode, handleImage, pipeImage, pivotImage, spoutImage ) {
+
+    var DEBUG_ORIGIN = true;
+
+    // where the fluid should come out of the unscaled spout image.
+    var SPOUT_OUTPUT_CENTER_X = 85;
+    var SPOUT_OUTPUT_WIDTH = 42;
 
     function FaucetNode( faucet, mvt ) {
 
@@ -35,7 +42,31 @@ define(
       this.addChild( pivotNode );
       this.addChild( spoutNode );
 
+      // origin
+      if ( DEBUG_ORIGIN ) {
+        this.addChild( new DebugOriginNode( "red" ) );
+      }
+
       // layout
+      {
+        // move spout's origin to the center of it's output
+        spoutNode.x = -SPOUT_OUTPUT_CENTER_X;
+        spoutNode.y = -spoutNode.height;
+        // pipe connects to left edge of spout
+        pipeNode.x = spoutNode.getLeft() - pipeNode.width;
+        pipeNode.y = spoutNode.getTop();
+        // pivot is on top of spout
+        pivotNode.x = spoutNode.getLeft() + ( 0.25 * spoutNode.width );
+        pivotNode.y = spoutNode.getTop() - pivotNode.height;
+        // butt end of handle is centered in pivot
+        handleNode.x = pivotNode.getCenterX();
+        handleNode.y = pivotNode.getCenterY() - ( handleNode.height / 2 );
+      }
+
+      // move to model location
+      var location = mvt.modelToView( faucet.location );
+      this.x = location.x;
+      this.y = location.y;
     }
 
     Inheritance.inheritPrototype( FaucetNode, Node ); // prototype chaining
