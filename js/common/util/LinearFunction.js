de583c0d80d9ea57e09a7c4ad9de443e9a273007
Chris Malley <cmalley@pixelzoom.com>
2013-03-08 10:38:36 -0700

    partial work on faucet handler drag handler, not working yet

---
+++ js/common/util/LinearFunction.js
@@ -0,0 +1,35 @@
+// Copyright 2002-2013, University of Colorado
+
+/**
+ * Function for doing a linear mapping between ranges.
+ *
+ * @author Chris Malley (PixelZoom, Inc.)
+ */
+define(
+  [],
+  function () {
+
+    /**
+     * @param {Range} range1
+     * @param {Range} range2
+     * @constructor
+     */
+    function LinearFunction( range1, range2 ) {
+
+      // Maps from range1 to range2.
+      this.evaluate = function( value ) {
+        var output = -range1.min + value;
+        output = output * range2.getLength() / range1.getLength();
+        output = range2.min + output;
+        return output;
+      };
+
+      // Maps from range2 to range1.
+      this.evaluateInverse = function ( value ) {
+        return new LinearFunction( range2, range1 ).evaluate( value );
+      };
+    }
+
+    return LinearFunction;
+
+  } );
--- js/concentration/view/FaucetNode.js
+++ js/concentration/view/FaucetNode.js
@@ -11,16 +11,18 @@ define(
   [
     "SCENERY/nodes/Node",
     "SCENERY/nodes/Image",
+    "SCENERY/input/SimpleDragHandler",
     "DOT/Vector2",
     "PHETCOMMON/math/Range",
     "PHETCOMMON/util/Inheritance",
     "common/view/DebugOriginNode",
+    "common/util/LinearFunction",
     "image!images/faucet_handle.png",
     "image!images/faucet_pipe.png",
     "image!images/faucet_pivot.png",
     "image!images/faucet_spout.png"
   ],
-  function ( Node, Image, Vector2, Range, Inheritance, DebugOriginNode, handleImage, pipeImage, pivotImage, spoutImage ) {
+  function ( Node, Image, SimpleDragHandler, Vector2, Range, Inheritance, DebugOriginNode, LinearFunction, handleImage, pipeImage, pivotImage, spoutImage ) {
 
     var DEBUG_ORIGIN = true;
 
@@ -28,14 +30,19 @@ define(
     var SPOUT_OUTPUT_CENTER_X = 85;
     var SPOUT_OUTPUT_WIDTH = 42;
 
-    var HANDLE_ORIENTATION_RANGE = new Range( -Math.PI/4, 0 ); // full off -> full on
+    var HANDLE_ORIENTATION_RANGE = new Range( -Math.PI / 4, 0 ); // full off -> full on
 
     function FaucetNode( faucet, mvt ) {
 
       Node.call( this ); // constructor stealing
 
+      console.log( "length=" + HANDLE_ORIENTATION_RANGE.getLength() );//XXX
+      var orientationToFlowRate = new LinearFunction( HANDLE_ORIENTATION_RANGE, new Range( 0, faucet.maxFlowRate ) );
+
       // child nodes
-      var handleNode = new Image( handleImage );
+      var handleNode = new Image( handleImage, {
+        cursor: "pointer"
+      } );
       var pipeNode = new Image( pipeImage );
       var pivotNode = new Image( pivotImage );
       var spoutNode = new Image( spoutImage );
@@ -69,6 +76,7 @@ define(
         // butt end of handle is centered in pivot
         handleNode.x = pivotNode.getCenterX();
         handleNode.y = pivotNode.getCenterY() - ( handleNode.height / 2 );
+        // handle is initially in the "off" orientation
         handleNode.rotateAround( new Vector2( pivotNode.getCenterX(), pivotNode.getCenterY() ), HANDLE_ORIENTATION_RANGE.min );
       }
 
@@ -76,6 +84,25 @@ define(
       var location = mvt.modelToView( faucet.location );
       this.x = location.x;
       this.y = location.y;
+
+      handleNode.addInputListener( new SimpleDragHandler(
+        {
+          translate: function( options ) {
+            var handleOrientation = HANDLE_ORIENTATION_RANGE.max; // TODO compute based on options.position
+            var flowRate = orientationToFlowRate.evaluate( handleOrientation );
+            faucet.flowRateProperty.set( flowRate );
+          }
+        } ) );
+
+      faucet.flowRateProperty.addObserver( function ( flowRate ) {
+        // butt end of handle is centered in pivot
+        handleNode.x = pivotNode.getCenterX();
+        handleNode.y = pivotNode.getCenterY() - ( handleNode.height / 2 );
+        // handle orientation matches flow rate
+        var orientation = orientationToFlowRate.evaluateInverse( flowRate );
+        console.log( "orientation=" + orientation );//XXX
+        handleNode.rotateAround( new Vector2( pivotNode.getCenterX(), pivotNode.getCenterY() ), HANDLE_ORIENTATION_RANGE.min );
+      } );
     }
 
     Inheritance.inheritPrototype( FaucetNode, Node ); // prototype chaining
