03433d9fe02359767233a72a2f35aac37029e842
Chris Malley <cmalley@pixelzoom.com>
2013-03-12 22:04:01 -0600

    port Concentration Meter model

---
+++ js/concentration/model/ConcentrationMeter.js
@@ -0,0 +1,42 @@
+// Copyright 2002-2013, University of Colorado
+
+/**
+ * Model of the concentration meter.
+ * <p/>
+ * NOTE: Determining when the probe is in one of the various fluids is handled in the view,
+ * where testing node intersections simplifies the process. Otherwise we'd need to
+ * model the shapes of the various fluids, an unnecessary complication.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+
+define(
+  [
+    "PHETCOMMON/model/property/Property",
+    "common/model/Movable"
+  ],
+  function( Property, Movable ) {
+
+    /**
+     * @param {Vector2} bodyLocation
+     * @param {Rectangle} bodyDragBounds
+     * @param {Vector2} probeLocation
+     * @param {Rectangle} probeDragBounds
+     * @constructor
+     */
+    function ConcentrationMeter( bodyLocation, bodyDragBounds, probeLocation, probeDragBounds ) {
+      this.value = new Property( NaN ); // NaN if the meter is not reading a value
+      this.body = new Movable( bodyLocation, bodyDragBounds );
+      this.probe = new Movable( probeLocation, probeDragBounds );
+    }
+
+    ConcentrationMeter.prototype.reset = function () {
+      this.value.reset();
+      this.body.reset();
+      this.probe.reset();
+    };
+
+    return ConcentrationMeter;
+
+  }
+);
