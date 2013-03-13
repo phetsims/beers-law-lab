5bb76773c0f392ca19cc8a17f02ce7d92bf50c4b
Chris Malley <cmalley@pixelzoom.com>
2013-03-13 11:56:40 -0600

    try CommonJS-style imports in BeakerNode

--- js/concentration/view/BeakerNode.js
+++ js/concentration/view/BeakerNode.js
@@ -1,102 +1,107 @@
 // Copyright 2002-2013, University of Colorado
 
-define(
-  [
-    "KITE/Shape",
-    "SCENERY/nodes/Node",
-    "SCENERY/nodes/Path",
-    "SCENERY/nodes/Text",
-    "PHETCOMMON/util/Inheritance",
-    "common/util/StringUtils"
-  ],
-  function ( Shape, Node, Path, Text, Inheritance, StringUtils ) {
-    "use strict";
+/**
+ * Visual representation of a beaker that is filled to the top with a solution.
+ * Ticks on the left edge of the beaker. Origin is at the bottom center.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+define( function ( require ) {
+  "use strict";
 
-    /**
-     * Constructor
-     * @param {Beaker} beaker
-     * @param {ModelViewTransform2D} mvt
-     * @param strings
-     * @constructor
-     */
-    function BeakerNode( beaker, mvt, strings ) {
+  // imports
+  var Shape = require( "KITE/Shape" );
+  var Node = require( "SCENERY/nodes/Node" );
+  var Path = require( "SCENERY/nodes/Path" );
+  var Text = require( "SCENERY/nodes/Text" );
+  var Inheritance = require( "PHETCOMMON/util/Inheritance" );
+  var StringUtils = require( "common/util/StringUtils" );
 
-      Node.call( this ); // constructor stealing
+  // constants
+  var MAX_VOLUME = 1;
+  var RIM_OFFSET = 20;
+  var MINOR_TICK_SPACING = 0.1; // L
+  var MINOR_TICKS_PER_MAJOR_TICK = 5;
+  var MAJOR_TICK_LENGTH = 30;
+  var MINOR_TICK_LENGTH = 15;
+  var TICK_LABEL_X_SPACING = 8;
+  var MAJOR_TICK_LABELS = new Array( "\u00bd", "1" ); // 1/2, 1
 
-      // constants
-      var MAX_VOLUME = 1;
-      var RIM_OFFSET = 20;
-      var MINOR_TICK_SPACING = 0.1; // L
-      var MINOR_TICKS_PER_MAJOR_TICK = 5;
-      var MAJOR_TICK_LENGTH = 30;
-      var MINOR_TICK_LENGTH = 15;
-      var TICK_LABEL_X_SPACING = 8;
-      var MAJOR_TICK_LABELS = new Array( "\u00bd", "1" ); // 1/2, 1
+  /**
+   * Constructor
+   * @param {Beaker} beaker
+   * @param {ModelViewTransform2D} mvt
+   * @param strings
+   * @constructor
+   */
+  function BeakerNode( beaker, mvt, strings ) {
 
-      // outline of the beaker, starting from upper left
-      var width = mvt.modelToView( beaker.size.width );
-      var height = mvt.modelToView( beaker.size.height );
-      var outlineShape = new Shape()
-        .moveTo( -(width / 2 ) - RIM_OFFSET, -height - RIM_OFFSET )
-        .lineTo( -(width / 2), -height )
-        .lineTo( -(width / 2), 0 )
-        .lineTo( width / 2, 0 )
-        .lineTo( width / 2, -height )
-        .lineTo( (width / 2) + RIM_OFFSET, -height - RIM_OFFSET );
-      this.addChild( new Path(
+    Node.call( this ); // constructor stealing
+
+    // outline of the beaker, starting from upper left
+    var width = mvt.modelToView( beaker.size.width );
+    var height = mvt.modelToView( beaker.size.height );
+    var outlineShape = new Shape()
+      .moveTo( -(width / 2 ) - RIM_OFFSET, -height - RIM_OFFSET )
+      .lineTo( -(width / 2), -height )
+      .lineTo( -(width / 2), 0 )
+      .lineTo( width / 2, 0 )
+      .lineTo( width / 2, -height )
+      .lineTo( (width / 2) + RIM_OFFSET, -height - RIM_OFFSET );
+    this.addChild( new Path(
+      {
+        shape: outlineShape,
+        stroke: "black",
+        lineWidth: 3,
+        lineCap: "round",
+        lineJoin: "round"
+      } ) );
+
+    // horizontal tick marks, left edge, from bottom up
+    var ticksParent = new Node();
+    this.addChild( ticksParent );
+    var numberOfTicks = Math.round( MAX_VOLUME / MINOR_TICK_SPACING );
+    var deltaY = height / numberOfTicks;
+    for ( var i = 1; i <= numberOfTicks; i++ ) {
+
+      // tick
+      var isMajorTick = ( i % MINOR_TICKS_PER_MAJOR_TICK == 0 );
+      var y = -( i * deltaY );
+      var leftX = -width / 2;
+      var rightX = leftX + ( isMajorTick ? MAJOR_TICK_LENGTH : MINOR_TICK_LENGTH );
+      var tickShape = new Shape().moveTo( leftX, y ).lineTo( rightX, y );
+      var tickPath = new Path(
         {
-          shape: outlineShape,
+          shape: tickShape,
           stroke: "black",
-          lineWidth: 3,
-          lineCap: "round",
-          lineJoin: "round"
-        } ) );
+          lineWidth: 2,
+          lineCap: "butt",
+          lineJoin: "bevel"
+        } );
+      ticksParent.addChild( tickPath );
 
-      // horizontal tick marks, left edge, from bottom up
-      var ticksParent = new Node();
-      this.addChild( ticksParent );
-      var numberOfTicks = Math.round( MAX_VOLUME / MINOR_TICK_SPACING );
-      var deltaY = height / numberOfTicks;
-      for ( var i = 1; i <= numberOfTicks; i++ ) {
-
-        // tick
-        var isMajorTick = ( i % MINOR_TICKS_PER_MAJOR_TICK == 0 );
-        var y = -( i * deltaY );
-        var leftX = -width / 2;
-        var rightX = leftX + ( isMajorTick ? MAJOR_TICK_LENGTH : MINOR_TICK_LENGTH );
-        var tickShape = new Shape().moveTo( leftX, y ).lineTo( rightX, y );
-        var tickPath = new Path(
-          {
-            shape: tickShape,
-            stroke: "black",
-            lineWidth: 2,
-            lineCap: "butt",
-            lineJoin: "bevel"
-          } );
-        ticksParent.addChild( tickPath );
-
-        // major tick label
-        if ( isMajorTick ) {
-          var labelIndex = ( i / MINOR_TICKS_PER_MAJOR_TICK ) - 1;
-          if ( labelIndex < MAJOR_TICK_LABELS.length ) {
-            var label = StringUtils.format( strings.pattern_0value_1units, [MAJOR_TICK_LABELS[labelIndex], strings.units_liters] );
-            ticksParent.addChild( new Text( label, {
-              font: "24px Arial",
-              fill: "black",
-              x: rightX + TICK_LABEL_X_SPACING,
-              centerY: tickPath.centerY
-            } ) );
-          }
+      // major tick label
+      if ( isMajorTick ) {
+        var labelIndex = ( i / MINOR_TICKS_PER_MAJOR_TICK ) - 1;
+        if ( labelIndex < MAJOR_TICK_LABELS.length ) {
+          var label = StringUtils.format( strings.pattern_0value_1units, [MAJOR_TICK_LABELS[labelIndex], strings.units_liters] );
+          ticksParent.addChild( new Text( label, {
+            font: "24px Arial",
+            fill: "black",
+            x: rightX + TICK_LABEL_X_SPACING,
+            centerY: tickPath.centerY
+          } ) );
         }
       }
-
-      var location = mvt.modelToView( beaker.location );
-      this.x = location.x;
-      this.y = location.y;
     }
 
-    // prototype chaining
-    Inheritance.inheritPrototype( BeakerNode, Node );
+    var location = mvt.modelToView( beaker.location );
+    this.x = location.x;
+    this.y = location.y;
+  }
 
-    return BeakerNode;
-  } );
+  // prototype chaining
+  Inheritance.inheritPrototype( BeakerNode, Node );
+
+  return BeakerNode;
+} );
