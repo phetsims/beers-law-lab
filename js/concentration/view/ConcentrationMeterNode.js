b96d54ea106dffee3a105d6b8cdda6e8d2c36f4a
Chris Malley <cmalley@pixelzoom.com>
2013-03-17 17:25:18 -0600

    update

--- js/concentration/view/ConcentrationMeterNode.js
+++ js/concentration/view/ConcentrationMeterNode.js
@@ -35,12 +35,12 @@ define( function( require ) {
   var probeImage = require( "image!images/concentration-meter-probe.png" );
 
   // constants
-  var VALUE_FORMAT = "0.000";
+  var VALUE_FORMAT = "0.000"; //TODO handle this better
   var NO_VALUE = "-";
-  var TITLE_CENTER_Y = 26; // specific to body image file
-  var TEXT_X_MARGIN = 25;  // specific to body image file
-  var VALUE_X_MARGIN = 25; // specific to body image file
-  var VALUE_CENTER_Y = 80; // specific to body image file
+  var TITLE_CENTER_Y = 26; // specific to bodyCenterImage
+  var TEXT_X_MARGIN = 25;  // specific to bodyCenterImage
+  var VALUE_X_MARGIN = 25; // specific to bodyCenterImage
+  var VALUE_CENTER_Y = 80; // specific to bodyCenterImage
 
   /**
    * @param {ConcentrationMeter} meter
@@ -54,13 +54,14 @@ define( function( require ) {
 
     // text nodes
     var textStyle = {
-      "textAlign": "center",
-      "textBaseline": "middle"
+      textAlign: "center",
+      textBaseline: "middle"
     };
-    var titleNode = new Text( strings.concentration, _.extend( textStyle, { font: "bold 18px Arial", fill: Color.WHITE.toCSS() } ) );
-    var unitsNode = new Text( StringUtils.format( strings.pattern_parentheses_0text, [ strings.units_molesPerLiter ]), _.extend( textStyle, { font: "bold 14px Arial", fill: Color.WHITE.toCSS() } ) );
-    var valueNode = new Text( VALUE_FORMAT, _.extend( textStyle, { font: "24px Arial", fill: 'black' } ) );
+    var titleNode = new Text( strings.concentration, _.extend( textStyle, { font: "bold 18px Arial", fill: "white" } ) );
+    var unitsNode = new Text( StringUtils.format( strings.pattern_parentheses_0text, [ strings.units_molesPerLiter ]), _.extend( textStyle, { font: "bold 14px Arial", fill: "white" } ) );
+    var valueNode = new Text( VALUE_FORMAT, _.extend( textStyle, { font: "24px Arial", fill: "black" } ) );
 
+    //TODO maxTextWidth appears to be wider than expected
     // create a background that fits the text
     var maxTextWidth = Math.max( titleNode.width, Math.max( unitsNode.width, valueNode.width ) );
     var bodyWidth = ( 2 * TEXT_X_MARGIN ) + maxTextWidth;
