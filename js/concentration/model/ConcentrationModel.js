ca229cf95e49129b8750875c5c2fddcc1e2220aa
Chris Malley <cmalley@pixelzoom.com>
2013-03-20 16:48:31 -0600

    set initial location of shaker and dropper to center of beaker

--- js/concentration/model/ConcentrationModel.js
+++ js/concentration/model/ConcentrationModel.js
@@ -57,9 +57,9 @@ define( function ( require ) {
     model.solution = new ConcentrationSolution( model.solute, DEFAULT_SOLUTE_AMOUNT, SOLUTION_VOLUME_RANGE.defaultValue );
     model.beaker = new Beaker( new Vector2( 400, 550 ), new Dimension2( 600, 300 ), 1 );
     model.precipitate = new Precipitate( model.solution, model.beaker );
-    model.shaker = new Shaker( new Vector2( 340, 170 ), new Rectangle( 225, 50, 400, 160 ), 0.75 * Math.PI, model.solute, SHAKER_MAX_DISPENSING_RATE );
+    model.shaker = new Shaker( new Vector2( model.beaker.location.x, 170 ), new Rectangle( 225, 50, 400, 160 ), 0.75 * Math.PI, model.solute, SHAKER_MAX_DISPENSING_RATE );
     model.shakerParticles = new ShakerParticles( model.shaker, model.solution, model.beaker );
-    model.dropper = new Dropper( new Vector2( 375, 210 ), new Rectangle( 230, 205, 400, 30 ), model.solute, DROPPER_FLOW_RATE );
+    model.dropper = new Dropper( new Vector2( model.beaker.location.x, 210 ), new Rectangle( 230, 205, 400, 30 ), model.solute, DROPPER_FLOW_RATE );
     model.evaporator = new Evaporator( MAX_EVAPORATION_RATE, model.solution );
     model.solventFaucet = new Faucet( new Vector2( 150, 220 ), 40, 100, MAX_INPUT_FLOW_RATE );
     model.drainFaucet = new Faucet( new Vector2( 790, 607 ), 40, 5, MAX_OUTPUT_FLOW_RATE );
