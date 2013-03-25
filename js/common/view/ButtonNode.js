c4ac7ff0dba935f21238640fb2edba4f0da9ccb9
Chris Malley <cmalley@pixelzoom.com>
2013-03-25 12:03:28 -0600

    demonstrate using custom icons with bootstrap

--- js/common/view/ButtonNode.js
+++ js/common/view/ButtonNode.js
@@ -18,7 +18,7 @@ define( function ( require ) {
    */
   function ButtonNode( text, callback ) {
 
-    var $button = $( '<button class="btn"><p style="font: 16px arial;">' + text + '</p></button>' );
+    var $button = $( '<button class="btn"><i class="bll-shaker-icon"></i>' + text + '</button>' );
     $button.bind( 'click', callback );
 
     DOM.call( this, $button[0] );
