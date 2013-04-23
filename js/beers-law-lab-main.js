9a2af36f40f23270c946cf67fcff434ca05caf9d
Chris Malley <cmalley@pixelzoom.com>
2013-04-23 12:09:41 -0600

    add has.js to contrib/ and html, disable assertions in config.js

--- js/beers-law-lab-main.js
+++ js/beers-law-lab-main.js
@@ -10,9 +10,11 @@ require(
     "JOIST/Sim",
     "concentration/ConcentrationTab",
     "beerslaw/BeersLawTab",
+    "SCENERY/util/Util",
     "i18n!../nls/beers-law-lab-strings"
   ],
-  function ( Sim, ConcentrationTab, BeersLawTab, strings ) {
+  function ( Sim, ConcentrationTab, BeersLawTab, Util, strings ) {
     "use strict";
+    console.log( Util.testAssert() ); // determine whether assertions are enabled in scenery
     new Sim( strings.beersLawLab, [ new ConcentrationTab( strings ), new BeersLawTab( strings ) ] ).start();
   } );
