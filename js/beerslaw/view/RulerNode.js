b6b2c3039112fe64627afbebdffe658465541b8f
Chris Malley <cmalley@pixelzoom.com>
2013-04-30 14:36:07 -0600

    intermediate checkin

--- js/beerslaw/view/RulerNode.js
+++ js/beerslaw/view/RulerNode.js
@@ -11,7 +11,9 @@ define( function ( require ) {
   var assert = require( 'ASSERT/assert' )( 'beers-law-lab' );   //TODO BLL specific
   var inherit = require( "PHET_CORE/inherit" );
   var Node = require( "SCENERY/nodes/Node" );
+  var Path = require( "SCENERY/nodes/Path" );
   var Rectangle = require( "SCENERY/nodes/Rectangle" );
+  var Shape = require( "KITE/Shape" );
 
   function RulerNode( width, height, majorTickLabels, units, options ) {
 
@@ -45,10 +47,29 @@ define( function ( require ) {
 
     // background
     thisNode.addChild( new Rectangle( 0, 0, width, height,
-                                      { fill: options.backgroundFill, stroke: options.backgroundStroke } ) );
+                                      { fill: options.backgroundFill,
+                                        stroke: options.backgroundStroke,
+                                        lineWidth: options.backgroundLineWidth } ) );
   }
 
   inherit( RulerNode, Node );
+
+  /**
+   * Creates a tick mark at a specific x location.
+   * Each tick is marked at the top and bottom of the ruler.
+   * If you desire a different style of tick mark, override this method.
+   *
+   * @param x
+   * @param rulerHeight
+   * @param tickHeight
+   * @param stroke
+   * @param lineWidth
+   * @return {Node}
+   */
+  RulerNode.prototype.createTickMarkNode = function ( x, rulerHeight, tickHeight, stroke, lineWidth ) {
+    var shape = new Shape().moveTo( x, 0 ).lineTo( x, tickHeight ).moveTo( x, rulerHeight - tickHeight ).lineTo( x, rulerHeight );
+    return new Path( { stroke: stroke, lineWidth: lineWidth, shape: shape } );
+  };
 
   return RulerNode;
 } );
