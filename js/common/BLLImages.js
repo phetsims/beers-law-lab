c982c495cc7f01f6e57cdb689a30c511df0a4c5c
Chris Malley <cmalley@pixelzoom.com>
2013-05-06 22:51:49 -0600

    authors

--- js/common/BLLImages.js
+++ js/common/BLLImages.js
@@ -6,6 +6,9 @@
  * This object will be extended lazily after the image loader completes.
  * Makes it possible to load through the module system rather having to
  * pass as a parameter everywhere or resort to using a global.
+ *
+ * @author Sam Reid
+ * @author Chris Malley (PixelZoom, Inc.)
  */
 define( function () {
   "use strict";
