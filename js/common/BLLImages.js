e0f511652e7db8d581ffd3a599b96df76aa5575d
Chris Malley <cmalley@pixelzoom.com>
2013-05-06 22:51:30 -0600

    simplify

--- js/common/BLLImages.js
+++ js/common/BLLImages.js
@@ -1,14 +1,13 @@
 // Copyright 2002-2013, University of Colorado
 
 /**
+ * Images loader for this simulation.
+ * <p>
  * This object will be extended lazily after the image loader completes.
  * Makes it possible to load through the module system rather having to
- * pass as parameter everywhere or resort to using a global.
+ * pass as a parameter everywhere or resort to using a global.
  */
-define( function( require ) {
+define( function () {
   "use strict";
-
-  var BLLImages = {};
-
-  return BLLImages;
+  return {};
 } );
