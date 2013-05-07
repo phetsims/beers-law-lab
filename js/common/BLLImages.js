b1d42019bcd19808a5361b4f49a096b1ae0e9d82
Chris Malley <cmalley@pixelzoom.com>
2013-05-06 22:49:32 -0600

    update

--- js/common/BLLImages.js
+++ js/common/BLLImages.js
@@ -2,7 +2,8 @@
 
 /**
  * This object will be extended lazily after the image loader completes.
- * Makes it possible to load through the module system rather than passed as parameter everywhere or used as global.
+ * Makes it possible to load through the module system rather than passed
+ * as parameter everywhere or used as global.
  */
 define( function( require ) {
   "use strict";
