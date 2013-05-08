6283ef43e356cbe112fbf7ffcd5f6384734eaaf0
Chris Malley <cmalley@pixelzoom.com>
2013-05-08 17:53:02 -0600

    fix jshint issues

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,44 +28,74 @@ module.exports = function( grunt ) {
           simFiles: [ '*.js', 'js/**/*.js' ],
           // source files from common-code dependencies
           commonFiles: [
-            '../assert/js/**/*.js',
-            '../dot/js/**/*.js',
-            '../fort/js/**/*.js',
-            '../joist/js/**/*.js',
-            '../kite/js/**/*.js',
-            '../phet-core/js/**/*.js',
+//            '../assert/js/**/*.js',
+//            '../dot/js/**/*.js',
+//            '../fort/js/**/*.js',
+//            '../joist/js/**/*.js',
+//            '../kite/js/**/*.js',
+//            '../phet-core/js/**/*.js',
             '../phetcommon/js/**/*.js',
-            '../scenery/js/**/*.js',
-            '../scenery-phet/js/**/*.js',
-            '../sun/js/**/*.js',
-            '../Watch.JS/src/**/*.js'
+//            '../scenery/js/**/*.js',
+//            '../scenery-phet/js/**/*.js',
+//            '../sun/js/**/*.js',
+//            '../Watch.JS/src/**/*.js'
           ],
           options: {
-            // options documented at http://www.jshint.com/docs/
-            //TODO standardize these, import them from a common file
-            curly: true,
+            // enforcing options
+            curly: true, // brackets for conditionals
             eqeqeq: true,
-            immed: false,
-            latedef: false,
+            immed: true,
+            latedef: true,
             newcap: true,
             noarg: true,
-            sub: true,
+            // noempty: true,
+            nonew: true,
+            // quotmark: 'single',
             undef: true,
-            boss: true,
-            eqnull: true,
-            browser: true,
-            node: true,
-            jquery: true,
-            expr: true,
+            // unused: true, // certain layer APIs not used in cases
+            // strict: true,
+
+            // relaxing options
+            es5: true, // we use ES5 getters and setters for now
+            loopfunc: true, // we know how not to shoot ourselves in the foot, and this is useful for _.each
+
+            expr: true, // so we can use assert && assert( ... )
+
             globals: {
-              Modernizr: true,
+              // for removal of assertions
+              sceneryAssert: true,
+              sceneryExtraAssert: true,
+
+              // for logging levels
+              sceneryLayerLog: true,
+              sceneryEventLog: true,
+
+              // for require.js
               define: true,
-              $: true,
-              _: true
+              require: true,
+
+              Uint16Array: false,
+              Uint32Array: false,
+              document: false,
+              window: false,
+              console: false,
+              Float32Array: true, // we actually polyfill this, so allow it to be set
+
+              HTMLImageElement: false,
+              HTMLCanvasElement: false,
+
+              $: false,
+              _: false,
+              clearTimeout: false,
+
+              // for DOM.js
+              Image: false,
+              Blob: false,
+
+              canvg: false
             }
           }
         }
-
       } );
 
   // Default task ('grunt')
