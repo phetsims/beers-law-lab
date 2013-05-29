87a9cf2cf716077bc85dcc9aafa2b51b11af843c
Chris Malley <cmalley@pixelzoom.com>
2013-05-29 14:44:11 -0600

    use Math.floor instead of round for interpolation of RGB components

--- js/common/util/ColorUtils.js
+++ js/common/util/ColorUtils.js
@@ -28,9 +28,9 @@ define( function( require ) {
     if ( distance < 0 || distance > 1 ) {
       throw new Error( "distance must be between 0 and 1: " + distance );
     }
-    var r = Math.round( Util.linear( 0, color1.r, 1, color2.r, distance ) );
-    var g = Math.round( Util.linear( 0, color1.g, 1, color2.g, distance ) );
-    var b = Math.round( Util.linear( 0, color1.b, 1, color2.b, distance ) );
+    var r = Math.floor( Util.linear( 0, color1.r, 1, color2.r, distance ) );
+    var g = Math.floor( Util.linear( 0, color1.g, 1, color2.g, distance ) );
+    var b = Math.floor( Util.linear( 0, color1.b, 1, color2.b, distance ) );
     var a = Util.linear( 0, color1.a, 1, color2.a, distance );
     return new Color( r, g, b, a );
   };
