89d62651f3c571eeb12edad13117571aec771a37
Chris Malley <cmalley@pixelzoom.com>
2013-06-18 08:39:01 -0600

    add options to ArrowButton

--- js/common/view/ArrowButton.js
+++ js/common/view/ArrowButton.js
@@ -13,10 +13,6 @@ define( function( require ) {
   var Path = require( 'SCENERY/nodes/Path' );
   var Shape = require( 'KITE/Shape' );
 
-  // constants
-  var ENABLED_COLOR = 'black';
-  var DISABLED_COLOR = 'rgb(175,175,175)';
-
   /**
    * @param {ArrowButton.Direction} direction
    * @param callback
@@ -25,29 +21,35 @@ define( function( require ) {
    */
   function ArrowButton( direction, callback, options ) {
 
+    var thisButton = this;
+
     var DEFAULT_ARROW_WIDTH = 20;
     options = _.extend( {
         arrowHeight: DEFAULT_ARROW_WIDTH,
         arrowWidth: DEFAULT_ARROW_WIDTH * Math.sqrt( 3 ) / 2,
         cornerRadius: 4,
-        xMargin: 7
+        xMargin: 7,
+        enabledFill: 'black',
+        disabledFill: 'rgb(175,175,175)'
       },
       options );
 
     var shape = ( direction === ArrowButton.Direction.LEFT ) ?
                 new Shape().moveTo( 0, 0 ).lineTo( options.arrowWidth, options.arrowHeight / 2 ).lineTo( 0, options.arrowHeight ).close() :
                 new Shape().moveTo( 0, options.arrowHeight / 2 ).lineTo( options.arrowWidth, 0 ).lineTo( options.arrowWidth, options.arrowHeight ).close();
-    var path = new Path( { fill: ENABLED_COLOR, shape: shape } );
+    var path = new Path( { fill: options.enabledFill, shape: shape } );
     Button.call( this, path, callback, options );
 
-    this.setEnabled = function( enabled ) {
-      path.fill = enabled ? ENABLED_COLOR : DISABLED_COLOR;
-      //TODO change Button stroke
-    }
+    thisButton.setEnabled = function( enabled ) {
+      path.fill = enabled ? options.enabledFill : options.disabledFill;
+      thisButton.pickable = enabled; //TODO workaround for lack of Button.enabled
+    };
+    thisButton.setEnabled( true );
   }
 
   inherit( Button, ArrowButton );
 
+  // direction that the arrow points
   ArrowButton.Direction = {
     'LEFT': 0,
     'RIGHT': 1
