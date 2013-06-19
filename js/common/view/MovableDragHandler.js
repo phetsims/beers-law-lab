1f2be208bfa4cde1ebe4bdf3b946c757d8e9ebfc
Chris Malley <cmalley@pixelzoom.com>
2013-06-19 11:28:43 -0600

    use parent coordinate frame in MovableDragHandler, solves drag "drift" problem

--- js/common/view/MovableDragHandler.js
+++ js/common/view/MovableDragHandler.js
@@ -19,8 +19,10 @@ define( function( require ) {
    */
   function MovableDragHandler( movable, mvt ) {
 
-    var startOffset; // where the drag started, relative to the movable's origin, in global view coordinates
-    var dragBounds = movable.dragBounds ? mvt.modelToViewBounds( movable.dragBounds ) : null; // drag bounds in global view coordinates
+    var startOffset; // where the drag started, relative to the movable's origin, in parent view coordinates
+    var dragBounds = movable.dragBounds ? mvt.modelToViewBounds( movable.dragBounds ) : null; // drag bounds in parent view coordinates
+
+    var target = null; //TODO workaround for scenery#66 (currentTarget is null in drag function)
 
     SimpleDragHandler.call( this, {
 
@@ -28,13 +30,14 @@ define( function( require ) {
 
       // note where the drag started
       start: function( event ) {
+        target = event.currentTarget; // save drag target, since it's null in drag()
         var location = mvt.modelToViewPosition( movable.location.get() );
-        startOffset = event.pointer.point.minus( location );
+        startOffset = target.globalToParentPoint( event.pointer.point ).minus( location );
       },
 
       // change the location, adjust for starting offset, constrain to drag bounds
       drag: function( event ) {
-        var location = event.pointer.point.minus( startOffset );
+        var location = target.globalToParentPoint( event.pointer.point ).minus( startOffset );
         if ( !dragBounds || dragBounds.containsPoint( location ) ) {
            movable.location.set( mvt.viewToModelPosition( location ) );
         }
