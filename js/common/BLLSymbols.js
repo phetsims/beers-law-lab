93c274e85576ed2a08ac9dd3b63ce64c7d54fe26
Chris Malley <cmalley@pixelzoom.com>
2013-07-11 13:09:17 -0600

    make BLLSymbols return an object literal, #14

--- js/common/BLLSymbols.js
+++ js/common/BLLSymbols.js
@@ -12,17 +12,15 @@ define( function( require ) {
   var toSubscript = require( "NITROGLYCERIN/ChemUtils" ).toSubscript;
   var BLLStrings = require( "common/BLLStrings" );
 
-  function BLLSymbols() {}
-
-  BLLSymbols.COBALT_II_NITRATE = toSubscript( "Co(NO3)2" );
-  BLLSymbols.COBALT_CHLORIDE = toSubscript( "CoCl2" );
-  BLLSymbols.COPPER_SULFATE = toSubscript( "CuSO4" );
-  BLLSymbols.DRINK_MIX = BLLStrings.drinkMix;
-  BLLSymbols.NICKEL_II_CHLORIDE = toSubscript( "NiCl2" );
-  BLLSymbols.POTASSIUM_CHROMATE = toSubscript( "K2CrO4" );
-  BLLSymbols.POTASSIUM_DICHROMATE = toSubscript( "K2Cr2O7" );
-  BLLSymbols.POTASSIUM_PERMANGANATE = toSubscript( "KMnO4" );
-  BLLSymbols.WATER = toSubscript( "H2O" );
-
-  return BLLSymbols;
+  return {
+    COBALT_II_NITRATE: toSubscript( "Co(NO3)2" ),
+    COBALT_CHLORIDE: toSubscript( "CoCl2" ),
+    COPPER_SULFATE: toSubscript( "CuSO4" ),
+    DRINK_MIX: BLLStrings.drinkMix,
+    NICKEL_II_CHLORIDE: toSubscript( "NiCl2" ),
+    POTASSIUM_CHROMATE: toSubscript( "K2CrO4" ),
+    POTASSIUM_DICHROMATE: toSubscript( "K2Cr2O7" ),
+    POTASSIUM_PERMANGANATE: toSubscript( "KMnO4" ),
+    WATER: toSubscript( "H2O" )
+  };
 } );
