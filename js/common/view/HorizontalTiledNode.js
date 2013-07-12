d4dbac5b85ab3f4d69ccde6ac4b4043bd2953919
Chris Malley <cmalley@pixelzoom.com>
2013-07-11 19:20:17 -0600

    add options, pass to supertype

--- js/common/view/HorizontalTiledNode.js
+++ js/common/view/HorizontalTiledNode.js
@@ -18,38 +18,41 @@ define( function( require ) {
   var Pattern = require( 'SCENERY/util/Pattern' );
   var Rectangle = require( 'SCENERY/nodes/Rectangle' );
 
-  // constants
-  var X_OVERLAP = 1; // overlap between tiles, to hide seams
-
   /**
    * @param {Number} totalWidth
-   * @param {Node} leftImage
-   * @param {Node} centerImage
-   * @param {Node} rightImage
+   * @param leftImage
+   * @param centerImage
+   * @param rightImage
+   * @param options
    * @constructor
    */
-  function HorizontalTiledNode( totalWidth, leftImage, centerImage, rightImage ) {
+  function HorizontalTiledNode( totalWidth, leftImage, centerImage, rightImage, options ) {
 
-    assert && assert( leftImage.height === centerImage.height && centerImage.height === rightImage.height );
+    assert && assert( leftImage.height === centerImage.height && centerImage.height === rightImage.height ); // all images are the same height
     assert && assert( ( leftImage.width + rightImage.width ) <= totalWidth );  // center may be unused
 
-    var thisNode = this;
-    Node.call( thisNode );
+    options = _.extend( {
+      xOverlap: 1  // how much the center overlaps with the left and right
+    }, options );
+
+    Node.call( this );
 
     // left
     var leftNode = new Image( leftImage );
-    thisNode.addChild( leftNode );
+    this.addChild( leftNode );
 
     // right
     var rightNode = new Image( rightImage );
     rightNode.right = totalWidth;
-    thisNode.addChild( rightNode );
+    this.addChild( rightNode );
 
     // tile the center, with overlap between tiles to hide seams
-    var tiledWidth = totalWidth - leftNode.width - rightNode.width + ( 2 * X_OVERLAP );
+    var tiledWidth = totalWidth - leftNode.width - rightNode.width + ( 2 * options.xOverlap );
     var centerNode = new Rectangle( 0, 0, tiledWidth, centerImage.height, { fill: new Pattern( centerImage ) } );
-    centerNode.left = leftNode.right - X_OVERLAP;
-    thisNode.addChild( centerNode );
+    centerNode.left = leftNode.right - options.xOverlap;
+    this.addChild( centerNode );
+
+    this.mutate( options );
   }
 
   inherit( Node, HorizontalTiledNode );
