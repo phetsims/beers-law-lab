5c8280542378e2fb956603d49fe2900a8748a3dc
Chris Malley <cmalley@pixelzoom.com>
2013-10-29 11:43:31 -0600

    joist#69, convert to different requirejs style for main.js

--- js/beers-law-lab-main.js
+++ js/beers-law-lab-main.js
@@ -5,32 +5,39 @@
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
-require( [ 'JOIST/SimLauncher', 'JOIST/Sim', 'BEERS_LAW_LAB/concentration/ConcentrationScreen', 'BEERS_LAW_LAB/beerslaw/BeersLawScreen',
-  'string!BEERS_LAW_LAB/beers-law-lab.name'],
-  function( SimLauncher, Sim, ConcentrationScreen, BeersLawScreen, titleString ) {
-    'use strict';
+define( function( require ) {
+  'use strict';
 
-    //TODO i18n, see joist#66
-    var simOptions = {
-      credits: 'PhET Development Team -\n' +
-               'Lead Design: Julia Chamberlain\n' +
-               'Software Development: Chris Malley\n' +
-               'Design Team: Kelly Lancaster, Emily B. Moore, Ariel Paul, Kathy Perkins',
-      thanks: 'Thanks -\n' +
-              'Conversion of this simulation to HTML5 was funded by the Royal Society of Chemistry.'
-    };
+  // imports
+  var BeersLawScreen = require( 'BEERS_LAW_LAB/beerslaw/BeersLawScreen' );
+  var ConcentrationScreen = require( 'BEERS_LAW_LAB/concentration/ConcentrationScreen' );
+  var Sim = require( 'JOIST/Sim');
+  var SimLauncher = require( 'JOIST/SimLauncher' );
 
-    // Appending '?dev' to the URL will enable developer-only features.
-    if ( window.phetcommon.getQueryParameter( 'dev' ) ) {
-      simOptions = _.extend( {
-        // add dev-specific options here
-        showHomeScreen: false,
-        screenIndex: 0
-      }, simOptions );
-    }
+  // strings
+  var simTitle = require( 'string!BEERS_LAW_LAB/beers-law-lab.name' );
 
-    SimLauncher.launch( function() {
-      var sim = new Sim( titleString, [ new ConcentrationScreen(), new BeersLawScreen() ], simOptions );
-      sim.start();
-    } );
+  //TODO i18n, see joist#66
+  var simOptions = {
+    credits: 'PhET Development Team -\n' +
+             'Lead Design: Julia Chamberlain\n' +
+             'Software Development: Chris Malley\n' +
+             'Design Team: Kelly Lancaster, Emily B. Moore, Ariel Paul, Kathy Perkins',
+    thanks: 'Thanks -\n' +
+            'Conversion of this simulation to HTML5 was funded by the Royal Society of Chemistry.'
+  };
+
+  // Appending '?dev' to the URL will enable developer-only features.
+  if ( window.phetcommon.getQueryParameter( 'dev' ) ) {
+    simOptions = _.extend( {
+      // add dev-specific options here
+      showHomeScreen: false,
+      screenIndex: 0
+    }, simOptions );
+  }
+
+  SimLauncher.launch( function() {
+    var sim = new Sim( simTitle, [ new ConcentrationScreen(), new BeersLawScreen() ], simOptions );
+    sim.start();
   } );
+} );
