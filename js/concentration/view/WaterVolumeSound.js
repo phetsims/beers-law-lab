bd19c0ec799782ae001fc817fa2281679200846a
jbphet <john.blanco@colorado.edu>
2015-02-09 12:27:06 -0700

    Started adding modulation to the sound for the volume of water in the beaker.

--- js/concentration/view/WaterVolumeSound.js
+++ js/concentration/view/WaterVolumeSound.js
@@ -5,6 +5,7 @@ define( function( require ) {
 
   // modules
   var inherit = require( 'PHET_CORE/inherit' );
+  var Timer = require( 'JOIST/Timer' );
 
   // constants
   var MIN_FREQUENCY = 263;
@@ -15,6 +16,8 @@ define( function( require ) {
    * @constructor
    */
   function WaterVolumeSound() {
+    var self = this;
+
     var audioContext;
     if ( window.AudioContext ) {
       audioContext = new window.AudioContext();
@@ -26,15 +29,28 @@ define( function( require ) {
     // create and initialize the audio nodes
     this.oscillator1 = audioContext.createOscillator(); // Create sound source
     this.oscillator1.type = 'sine';
+    this.lfoGainControl = audioContext.createGain();
+    this.lfoGainControl.gain.value = 1;
     this.gainControl = audioContext.createGain();
     this.gainControl.gain.value = 0;
 
     // hook up the audio nodes
-    this.oscillator1.connect( this.gainControl );
+    this.oscillator1.connect( this.lfoGainControl );
+    this.lfoGainControl.connect( this.gainControl );
     this.gainControl.connect( audioContext.destination );
 
     // start the oscillator
     this.oscillator1.start( 0 );
+
+    // set up the low frequency oscillation
+    //var counter = 0;
+    //Timer.setInterval( function(){
+    //  counter++;
+    //  self.lfoGainControl.gain.value = counter / 1000;
+    //  if ( counter > 1000 ){
+    //    counter = 0;
+    //  }
+    //}, 60 );
   }
 
   return inherit( Object, WaterVolumeSound, {
