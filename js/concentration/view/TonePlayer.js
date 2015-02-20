0746222a4f0345842c228304a8e1777b967e0fe5
jbphet <john.blanco@colorado.edu>
2015-02-20 14:42:20 -0700

    Added reverb to tone player, fixed an issue where the pitch took a while to get to the desired value.

--- js/concentration/view/TonePlayer.js
+++ js/concentration/view/TonePlayer.js
@@ -9,6 +9,11 @@ define( function( require ) {
 
   // modules
   var inherit = require( 'PHET_CORE/inherit' );
+  var Sound = require( 'VIBE/Sound' );
+  var Timer = require( 'JOIST/Timer' );
+
+  // audio
+  var impulseResponse = new Sound( require( 'audio!BEERS_LAW_LAB/stairwell_ortf.ogg' ) );
 
   // constants
   var MAX_GAIN = 0.75;
@@ -30,20 +35,38 @@ define( function( require ) {
     this.envelope = this.audioContext.createGain();
     this.envelope.gain.value = 0;
 
+    // create a convolver that will be used to add a bit of reverb to the sound
+    var reverb = this.audioContext.createConvolver();
+
     // interconnect the various audio nodes
     this.oscillator1.connect( this.envelope );
-    this.envelope.connect( this.audioContext.destination );
+    this.envelope.connect( reverb );
+    reverb.connect( this.audioContext.destination );
 
     // start the oscillator running
     this.oscillator1.start( 0 );
+
+    // Load the impulse response into the convolver. This is necessary because of the async load of the impulse
+    // response buffer.
+    var attemptLoad = function() {
+      if ( typeof( impulseResponse.audioBuffer ) !== 'undefined' ) {
+        reverb.buffer = impulseResponse.audioBuffer;
+        console.log( 'loaded' );
+      }
+      else {
+        Timer.setTimeout( 500, attemptLoad );
+        console.log( 'going around again' );
+      }
+      return false;
+    }();
   }
 
   return inherit( Object, TonePlayer, {
     play: function( frequency ) {
-      this.oscillator1.frequency.value = frequency;
       var now = this.audioContext.currentTime;
-      this.envelope.gain.linearRampToValueAtTime( MAX_GAIN, now + 0.030 );
-      this.envelope.gain.setTargetAtTime( 0, now + 0.3, 0.15 );
+      this.oscillator1.frequency.linearRampToValueAtTime( frequency, now );
+      this.envelope.gain.linearRampToValueAtTime( MAX_GAIN, now + 0.020 );
+      this.envelope.gain.setTargetAtTime( 0, now + 0.2, 0.1 );
     }
   } );
 } );
