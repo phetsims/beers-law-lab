dca1fae74588c43379443edc9ac481133cd3f488
Chris Malley <cmalley@pixelzoom.com>
2015-11-03 09:05:09 -0700

    fix probe touch areas in 1.3 branch, #119

--- js/concentration/view/ConcentrationMeterNode.js
+++ js/concentration/view/ConcentrationMeterNode.js
@@ -177,9 +177,7 @@ define( function( require ) {
     } );
 
     // touch area
-    var dx = 0.25 * thisNode.width;
-    var dy = 0.25 * thisNode.height;
-    thisNode.touchArea = Shape.rectangle( thisNode.x - dx, thisNode.y - dy, thisNode.width + dx + dx, thisNode.height + dy + dy );
+    thisNode.touchArea = thisNode.localBounds.dilatedXY( 0.25 * thisNode.width, 0.25 * thisNode.height );
 
     // drag handler
     this.movableDragHandler = new MovableDragHandler( probe.locationProperty, {
