f38a9c393e7d747d722fde0ad5229b3504829fcb
Chris Malley <cmalley@pixelzoom.com>
2016-01-31 11:53:34 -0700

    improve query parameter validation, #150

--- js/common/BLLQueryParameters.js
+++ js/common/BLLQueryParameters.js
@@ -13,22 +13,32 @@ define( function( require ) {
 
   var getQueryParameter = phet.chipper.getQueryParameter;
 
+  // constants - valid values, 0th element is the default
+  var CONCENTRATION_METER_UNITS_VALUES = [ 'molesPerLiter', 'percent' ];
+  var BEAKER_TICK_UNITS_VALUES = [ 'liters', 'milliliters' ];
+
   var BLLQueryParameters = {
 
     // {boolean} whether the solute amount (in grams) is visible on the Concentration screen, see beers-law-lab#148
     SOLUTE_AMOUNT_VISIBLE: !!getQueryParameter( 'soluteAmountVisible' ) || false,
 
     // {string} units on the concentration meter, 'molesPerLiter'|'percent', see beers-law-lab#149
-    CONCENTRATION_METER_UNITS: getQueryParameter( 'concentrationMeterUnits' ) || 'molesPerLiter',
+    CONCENTRATION_METER_UNITS: getQueryParameter( 'concentrationMeterUnits' ) || CONCENTRATION_METER_UNITS_VALUES[ 0 ],
 
     // {string} units for beaker ticks, 'liters'|'milliliters', see beers-law-lab#150
-    BEAKER_UNITS: getQueryParameter( 'beakerUnits' ) || 'liters'
+    BEAKER_UNITS: getQueryParameter( 'beakerUnits' ) || BEAKER_TICK_UNITS_VALUES[ 0 ]
   };
 
-  // validation
-  assert && assert( typeof BLLQueryParameters.SOLUTE_AMOUNT_VISIBLE === 'boolean' );
-  assert && assert( BLLQueryParameters.CONCENTRATION_METER_UNITS === 'molesPerLiter' || BLLQueryParameters.CONCENTRATION_METER_UNITS === 'percent' );
-  assert && assert( BLLQueryParameters.BEAKER_UNITS === 'liters' || BLLQueryParameters.BEAKER_UNITS === 'milliliters' );
+  // validation - use Error instead of assert, because these are user errors, not programming errors
+  if ( typeof BLLQueryParameters.SOLUTE_AMOUNT_VISIBLE !== 'boolean' ) {
+    throw new Error( 'invalid value for soluteAmountVisible query parameter: ' + BLLQueryParameters.SOLUTE_AMOUNT_VISIBLE );
+  }
+  if ( _.indexOf( CONCENTRATION_METER_UNITS_VALUES, BLLQueryParameters.CONCENTRATION_METER_UNITS ) === -1 ) {
+    throw new Error( 'invalid value for concentrationMeterUnits query parameter: ' + BLLQueryParameters.CONCENTRATION_METER_UNITS );
+  }
+  if ( _.indexOf( BEAKER_TICK_UNITS_VALUES, BLLQueryParameters.BEAKER_UNITS ) === -1 ) {
+    throw new Error( 'invalid value for beakerUnits query parameter: ' + BLLQueryParameters.BEAKER_UNITS );
+  }
 
   beersLawLab.register( 'BLLQueryParameters', BLLQueryParameters );
 
