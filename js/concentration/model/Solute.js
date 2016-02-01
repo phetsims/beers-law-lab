04a0d0490a3da0a6ed56c1ae3a00868beffa7035
Chris Malley <cmalley@pixelzoom.com>
2016-02-01 12:01:09 -0700

    use more accurate values for molar mass, change order of params in Solute constructor, #148

--- js/concentration/model/Solute.js
+++ js/concentration/model/Solute.js
@@ -1,4 +1,4 @@
-// Copyright 2013-2015, University of Colorado Boulder
+// Copyright 2013-2016, University of Colorado Boulder
 
 /**
  * Model of a solute, immutable.
@@ -31,13 +31,13 @@ define( function( require ) {
    * @param {string} name
    * @param {string} formula
    * @param {number} stockSolutionConcentration - mol/L
-   * @param {SoluteColorScheme} colorScheme
    * @param {number} molarMass - g/mol
+   * @param {SoluteColorScheme} colorScheme
    * @param {Tandem} tandem
    * @param {Object} [options]
    * @constructor
    */
-  function Solute( name, formula, stockSolutionConcentration, colorScheme, molarMass, tandem, options ) {
+  function Solute( name, formula, stockSolutionConcentration, molarMass, colorScheme, tandem, options ) {
 
     options = _.extend( {
       particleColor: colorScheme.maxColor,
@@ -48,8 +48,8 @@ define( function( require ) {
     this.name = name;
     this.formula = formula;
     this.stockSolutionConcentration = stockSolutionConcentration;
-    this.colorScheme = colorScheme;
     this.molarMass = molarMass;
+    this.colorScheme = colorScheme;
     this.particleColor = options.particleColor;
     this.particleSize = options.particleSize;
     this.particlesPerMole = options.particlesPerMole;
@@ -86,72 +86,73 @@ define( function( require ) {
   Solute.DRINK_MIX = new Solute(
     drinkMixString,
     drinkMixString,
-    5.5,
+    5.5, // stockSolutionConcentration, mol/L
+    342.296, // molarMass, g/mol (sucrose)
     new SoluteColorScheme( 0, new Color( 224, 255, 255 ), 0.05, new Color( 255, 225, 225 ), 5.96, new Color( 255, 0, 0 ) ),
-    342.3, // g/mol, source: used sucrose value from PubChem
     tandem.createTandem( 'drinkMix' )
   );
 
   Solute.COBALT_II_NITRATE = new Solute(
     cobaltIINitrateString,
     BLLSymbols.COBALT_II_NITRATE,
-    5.0,
+    5.0, // stockSolutionConcentration, mol/L
+    182.942, // molarMass, g/mol
     new SoluteColorScheme( 0, Solvent.WATER_COLOR, 0.05, new Color( 255, 225, 225 ), 5.64, new Color( 255, 0, 0 ) ),
-    182.9, // g/mol, source: Google
     tandem.createTandem( 'cobaltIINitrate' )
   );
 
   Solute.COBALT_CHLORIDE = new Solute(
     cobaltChlorideString,
     BLLSymbols.COBALT_CHLORIDE,
-    4.0,
+    4.0, // stockSolutionConcentration, mol/L
+    129.839, // molarMass, g/mol
     new SoluteColorScheme( 0, Solvent.WATER_COLOR, 0.05, new Color( 255, 242, 242 ), 4.33, new Color( 255, 106, 106 ) ),
-    129.9, // g/mol, source: Google
+
     tandem.createTandem( 'cobaltChloride' )
   );
 
   Solute.POTASSIUM_DICHROMATE = new Solute(
     potassiumDichromateString,
     BLLSymbols.POTASSIUM_DICHROMATE,
-    0.5,
+    0.5, // stockSolutionConcentration, mol/L
+    294.185, // molarMass, g/mol
     new SoluteColorScheme( 0, Solvent.WATER_COLOR, 0.01, new Color( 255, 204, 153 ), 0.51, new Color( 255, 127, 0 ) ),
-    294.1, // g/mol, source: Google
     tandem.createTandem( 'potassiumDichromate' )
   );
 
   Solute.POTASSIUM_CHROMATE = new Solute(
     potassiumChromateString,
     BLLSymbols.POTASSIUM_CHROMATE,
-    3.0,
+    3.0, // stockSolutionConcentration, mol/L
+    194.191, // molarMass, g/mol
     new SoluteColorScheme( 0, Solvent.WATER_COLOR, 0.05, new Color( 255, 255, 153 ), 3.35, new Color( 255, 255, 0 ) ),
-    194.2, // g/mol, source: Google
     tandem.createTandem( 'potassiumChromate' )
   );
 
   Solute.NICKEL_II_CHLORIDE = new Solute(
     nickelIIChlorideString,
     BLLSymbols.NICKEL_II_CHLORIDE,
-    5.0,
+    5.0, // stockSolutionConcentration, mol/L
+    129.599, // molarMass, g/mol
     new SoluteColorScheme( 0, Solvent.WATER_COLOR, 0.2, new Color( 170, 255, 170 ), 5.21, new Color( 0, 128, 0 ) ),
-    129.6, // g/mol, source: Google
     tandem.createTandem( 'nickelIIChloride' )
   );
 
   Solute.COPPER_SULFATE = new Solute(
     copperSulfateString,
     BLLSymbols.COPPER_SULFATE,
-    1.0,
+    1.0, // stockSolutionConcentration, mol/L
+    159.609, // molarMass, g/mol
     new SoluteColorScheme( 0, Solvent.WATER_COLOR, 0.2, new Color( 200, 225, 255 ), 1.38, new Color( 30, 144, 255 ) ),
-    159.6, // source: Google
     tandem.createTandem( 'copperSulfate' )
   );
 
   Solute.POTASSIUM_PERMANGANATE = new Solute(
     potassiumPermanganateString,
     BLLSymbols.POTASSIUM_PERMANGANATE,
-    0.4,
+    0.4, // stockSolutionConcentration, mol/L
+    158.034, // molarMass, g/mol
     new SoluteColorScheme( 0, Solvent.WATER_COLOR, 0.01, new Color( 255, 0, 255 ), 0.48, new Color( 80, 0, 120 ) ),
-    158.0, // g/mol, source: Google
     tandem.createTandem( 'potassiumPermanganate' ), {
       particleColor: Color.BLACK
     }
