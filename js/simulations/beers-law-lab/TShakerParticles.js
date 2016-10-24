5dedc47ce39f90eacd3b5d08f13e77b9d0e53c19
samreid <reids@colorado.edu>
2016-10-24 13:29:31 -0600

    Removed unused code, see https://github.com/phetsims/phet-io/issues/727

--- js/simulations/beers-law-lab/TShakerParticles.js
+++ js/simulations/beers-law-lab/TShakerParticles.js
@@ -13,7 +13,6 @@ define( function( require ) {
   var phetioNamespace = require( 'PHET_IO/phetioNamespace' );
   var phetioInherit = require( 'PHET_IO/phetioInherit' );
   var TObject = require( 'PHET_IO/types/TObject' );
-  var TVoid = require( 'PHET_IO/types/TVoid' );
   var phetio = require( 'PHET_IO/phetio' );
 
   var TShakerParticles = function( instance, phetioID ) {
@@ -21,18 +20,7 @@ define( function( require ) {
     assertInstanceOf( instance, phet.beersLawLab.ShakerParticles );
   };
 
-  phetioInherit( TObject, 'TShakerParticles', TShakerParticles, {
-
-    // TODO: rename this to a more specific name
-    // setValue: {
-    //   returnType: TVoid,
-    //   parameterTypes: [],
-    //   implementation: function() {
-    //     this.instance.removeAllParticles();
-    //   },
-    //   documentation: 'Set the formula of the solute'
-    // }
-  }, {
+  phetioInherit( TObject, 'TShakerParticles', TShakerParticles, {}, {
     toStateObject: function( instance ) {
 
       // TODO: Just returning a string from here doesn't work.... why?
