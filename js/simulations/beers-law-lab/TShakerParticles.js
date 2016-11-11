77e51c6602278953cef6f84f1a3aeeb470f0549b
samreid <reids@colorado.edu>
2016-11-11 15:45:06 -0700

    fixed lint issues

--- js/simulations/beers-law-lab/TShakerParticles.js
+++ js/simulations/beers-law-lab/TShakerParticles.js
@@ -13,7 +13,6 @@ define( function( require ) {
   var phetioNamespace = require( 'PHET_IO/phetioNamespace' );
   var phetioInherit = require( 'PHET_IO/phetioInherit' );
   var TObject = require( 'PHET_IO/types/TObject' );
-  var TVoid = require( 'PHET_IO/types/TVoid' );
   var phetio = require( 'PHET_IO/phetio' );
 
   var TShakerParticles = function( instance, phetioID ) {
