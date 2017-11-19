30fac4b14c4a8450b0894ba094640dcbf3da22c4
samreid <reids@colorado.edu>
2017-11-18 18:24:21 -0700

    Converted Tandem.createStaticTandem to Tandem.rootTandem.createTandem, see https://github.com/phetsims/tandem/issues/34 Converted Tandem.tandemOptional() to Tandem.optional Converted Tandem.tandemRequired() to Tandem.required

--- js/common/BLLConstants.js
+++ js/common/BLLConstants.js
@@ -24,8 +24,8 @@ define( function( require ) {
     SOLUTION_VOLUME_RANGE: new RangeWithValue( 0, BEAKER_VOLUME, 0.5 ), // liters
     BEAKER_VOLUME: BEAKER_VOLUME,
     DEFAULT_CUVETTE_SNAP_INTERVAL: 0.1, // cm
-    CONCENTRATION_SCREEN_TANDEM: Tandem.createStaticTandem( 'concentrationScreen' ),
-    BEERS_LAW_SCREEN_TANDEM: Tandem.createStaticTandem( 'beersLawScreen' )
+    CONCENTRATION_SCREEN_TANDEM: Tandem.rootTandem.createTandem( 'concentrationScreen' ),
+    BEERS_LAW_SCREEN_TANDEM: Tandem.rootTandem.createTandem( 'beersLawScreen' )
   };
 
   beersLawLab.register( 'BLLConstants', BLLConstants );
