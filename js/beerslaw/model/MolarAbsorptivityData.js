7d80c12a50bc2b63490e57798aba15376816910b
phet-dev <phetadmin@gmail.com>
2020-02-27 03:08:18 -0700

    update copyright dates from daily grunt work

--- js/beerslaw/model/MolarAbsorptivityData.js
+++ js/beerslaw/model/MolarAbsorptivityData.js
@@ -1,4 +1,4 @@
-// Copyright 2013-2019, University of Colorado Boulder
+// Copyright 2013-2020, University of Colorado Boulder
 
 /**
  * Container for molar absorptivity data for the visible spectrum of light.
