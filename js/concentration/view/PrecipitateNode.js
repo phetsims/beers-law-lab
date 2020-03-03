68d2160e8db14b61032a12116ed18a2b0996258f
phet-dev <phetadmin@gmail.com>
2020-03-03 03:10:15 -0700

    update copyright dates from daily grunt work

--- js/concentration/view/PrecipitateNode.js
+++ js/concentration/view/PrecipitateNode.js
@@ -1,4 +1,4 @@
-// Copyright 2013-2020, University of Colorado Boulder
+// Copyright 2020, University of Colorado Boulder
 
 /**
  * PrecipitateNode displays precipitate at the bottom of the beaker.
