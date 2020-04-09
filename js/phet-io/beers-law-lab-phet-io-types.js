5dcb48de5c8581bd0105ddc912508b7bb3904e5b
zepumph <michael.kauzmann@colorado.edu>
2020-04-09 11:33:20 -0800

    fix ReferenceIO typeName and phetioDocumentation, https://github.com/phetsims/tandem/issues/159#event-3218972885

--- js/phet-io/beers-law-lab-phet-io-types.js
+++ js/phet-io/beers-law-lab-phet-io-types.js
@@ -224,7 +224,7 @@ window.phet.preloads.phetio.phetioTypes = assert &&
       "events": [],
       "methodOrder": [],
       "methods": {},
-      "supertype": "ReferenceIO",
+      "supertype": "ReferenceIO<ObjectIO>",
       "typeName": "BeersLawSolutionIO"
     },
     "BooleanIO": {
@@ -1769,13 +1769,13 @@ window.phet.preloads.phetio.phetioTypes = assert &&
       "supertype": "ObjectIO",
       "typeName": "RangeIO"
     },
-    "ReferenceIO": {
-      "documentation": "Uses reference identity for toStateObject/fromStateObject and validates based on its parameter IO Type.",
+    "ReferenceIO<ObjectIO>": {
+      "documentation": "Uses reference identity for serializing and deserializing, and validates based on its parameter IO Type.",
       "events": [],
       "methodOrder": [],
       "methods": {},
       "supertype": "ObjectIO",
-      "typeName": "ReferenceIO"
+      "typeName": "ReferenceIO<ObjectIO>"
     },
     "RichTextIO": {
       "documentation": "The tandem IO type for the scenery RichText node",
@@ -1832,7 +1832,7 @@ window.phet.preloads.phetio.phetioTypes = assert &&
       "events": [],
       "methodOrder": [],
       "methods": {},
-      "supertype": "ReferenceIO",
+      "supertype": "ReferenceIO<ObjectIO>",
       "typeName": "ScreenIO"
     },
     "ShakerParticleIO": {
@@ -1890,7 +1890,7 @@ window.phet.preloads.phetio.phetioTypes = assert &&
           "returnType": "VoidIO"
         }
       },
-      "supertype": "ReferenceIO",
+      "supertype": "ReferenceIO<ObjectIO>",
       "typeName": "SoluteIO"
     },
     "SoluteParticleIO": {
