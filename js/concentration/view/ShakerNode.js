ddb07007dcac2b44da7490bc6cb3554ec4479f3b
pixelzoom <cmalley@pixelzoom.com>
2021-03-13 13:41:40 -0700

    remove hint arrows from shaker, https://github.com/phetsims/beers-law-lab/issues/268

--- js/concentration/view/ShakerNode.js
+++ js/concentration/view/ShakerNode.js
@@ -9,14 +9,12 @@
 import Property from '../../../../axon/js/Property.js';
 import merge from '../../../../phet-core/js/merge.js';
 import ModelViewTransform2 from '../../../../phetcommon/js/view/ModelViewTransform2.js';
-import ArrowNode from '../../../../scenery-phet/js/ArrowNode.js';
 import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
 import DragListener from '../../../../scenery/js/listeners/DragListener.js';
 import Circle from '../../../../scenery/js/nodes/Circle.js';
 import Image from '../../../../scenery/js/nodes/Image.js';
 import Node from '../../../../scenery/js/nodes/Node.js';
 import RichText from '../../../../scenery/js/nodes/RichText.js';
-import VBox from '../../../../scenery/js/nodes/VBox.js';
 import Tandem from '../../../../tandem/js/Tandem.js';
 import shakerImage from '../../../images/shaker_png.js';
 import beersLawLab from '../../beersLawLab.js';
@@ -24,14 +22,6 @@ import Shaker from '../model/Shaker.js';
 
 // constants
 const DEBUG_ORIGIN = false;
-const ARROW_LENGTH = 40;
-const ARROW_OPTIONS = {
-  tailWidth: 23,
-  headWidth: 40,
-  headHeight: 30,
-  fill: 'yellow',
-  stroke: 'rgb(160,160,160)'
-};
 
 class ShakerNode extends Node {
 
@@ -73,27 +63,8 @@ class ShakerNode extends Node {
       textPropertyOptions: { phetioReadOnly: true }
     } );
 
-    // hint arrows
-    const downArrowNode = new ArrowNode( 0, 0, 0, ARROW_LENGTH, ARROW_OPTIONS );
-    const upArrowNode = new ArrowNode( 0, 0, 0, -ARROW_LENGTH, ARROW_OPTIONS );
-
-    // Set visibility of this Node programmatically to show/hide the hint arrows.
-    const arrowsParent = new VBox( {
-      spacing: imageNode.height + 8,
-      children: [ upArrowNode, downArrowNode ],
-      center: imageNode.center,
-      pickable: false,
-      visible: false
-    } );
-
-    // Set visibility of this Node to totally hide the hint arrows via PhET-iO.
-    const hintArrowsNode = new Node( {
-      children: [ arrowsParent ],
-      tandem: options.tandem.createTandem( 'hintArrowsNode' )
-    } );
-
     // common parent, to simplify rotation and label alignment.
-    const parentNode = new Node( { children: [ imageNode, labelNode, hintArrowsNode ] } );
+    const parentNode = new Node( { children: [ imageNode, labelNode ] } );
     this.addChild( parentNode );
     parentNode.rotate( shaker.orientation - Math.PI ); // assumes that shaker points to the left in the image file
 
@@ -106,13 +77,9 @@ class ShakerNode extends Node {
     }
 
     // sync position with model
-    let shakerWasMoved = false;
     shaker.positionProperty.link( position => {
       this.translation = modelViewTransform.modelToViewPosition( position );
-      shakerWasMoved = true;
-      arrowsParent.visible = false;
     } );
-    shakerWasMoved = false; // reset to false, because function is fired when link is performed
 
     // sync visibility with model
     shaker.visibleProperty.link( visible => {
@@ -138,16 +105,6 @@ class ShakerNode extends Node {
       modelViewTransform: modelViewTransform,
       tandem: options.tandem.createTandem( 'dragListener' )
     } ) );
-
-    // Make the arrows visible when pointer is over this Node, until the shaker is moved.
-    this.addInputListener( {
-      enter: () => {
-        arrowsParent.visible = !shakerWasMoved;
-      },
-      exit: () => {
-        arrowsParent.visible = false;
-      }
-    } );
   }
 }
 
