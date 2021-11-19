11d4a404b946c2848d3b139d93aad7753f03bc13
Sam Reid <reids@colorado.edu>
2021-11-18 17:43:59 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/beersLawLabStrings.ts
@@ -0,0 +1,67 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import beersLawLab from './beersLawLab.js';
+
+type StringsType = {
+  'absorbance': string,
+  'beers-law-lab': {
+    'title': string
+  },
+  'cobaltIINitrate': string,
+  'cobaltChloride': string,
+  'concentration': string,
+  'copperSulfate': string,
+  'drinkMix': string,
+  'evaporation': string,
+  'preset': string,
+  'lots': string,
+  'nickelIIChloride': string,
+  'none': string,
+  'pattern': {
+    '0label': string,
+    '0value': {
+      '1units': string
+    },
+    '0percent': string,
+    '0formula': {
+      '1name': string
+    },
+    '0soluteAmount': string
+  },
+  'potassiumChromate': string,
+  'potassiumDichromate': string,
+  'potassiumPermanganate': string,
+  'removeSolute': string,
+  'saturated': string,
+  'sodiumChloride': string,
+  'solid': string,
+  'solute': string,
+  'solution': string,
+  'screen': {
+    'concentration': string,
+    'beersLaw': string
+  },
+  'transmittance': string,
+  'units': {
+    'centimeters': string,
+    'liters': string,
+    'milliliters': string,
+    'molesPerLiter': string,
+    'mM': string,
+    'uM': string,
+    'nm': string
+  },
+  'variable': string,
+  'wavelength': string
+};
+
+const beersLawLabStrings = getStringModule( 'BEERS_LAW_LAB' ) as StringsType;
+
+beersLawLab.register( 'beersLawLabStrings', beersLawLabStrings );
+
+export default beersLawLabStrings;
