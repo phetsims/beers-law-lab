230271d826a45cf4e07c89a6b0328fe1649c0b87
phet-dev <phetadmin@gmail.com>
2022-01-07 03:16:54 -0700

    update copyright dates from daily grunt work

--- js/concentration/model/SoluteForm.js
+++ js/concentration/model/SoluteForm.js
@@ -1,4 +1,4 @@
-// Copyright 2021, University of Colorado Boulder
+// Copyright 2021-2022, University of Colorado Boulder
 
 /**
  * SoluteForm is an enumeration of the forms that a solute may take in this sim.
