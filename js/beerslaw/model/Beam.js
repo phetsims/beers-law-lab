af8ee4f40a935f69dddcb1c7592f491be1898842
zepumph <michael.kauzmann@colorado.edu>
2022-03-04 13:08:27 -0700

    import kite files from imports.ts, https://github.com/phetsims/kite/issues/96

--- js/beerslaw/model/Beam.js
+++ js/beerslaw/model/Beam.js
@@ -15,7 +15,7 @@
 
 import DerivedProperty from '../../../../axon/js/DerivedProperty.js';
 import Utils from '../../../../dot/js/Utils.js';
-import Shape from '../../../../kite/js/Shape.js';
+import { Shape } from '../../../../kite/js/imports.js';
 import ModelViewTransform2 from '../../../../phetcommon/js/view/ModelViewTransform2.js';
 import VisibleColor from '../../../../scenery-phet/js/VisibleColor.js';
 import { LinearGradient } from '../../../../scenery/js/imports.js';
--- js/concentration/view/BLLDropperNode.js
+++ js/concentration/view/BLLDropperNode.js
@@ -9,7 +9,7 @@
 
 import Multilink from '../../../../axon/js/Multilink.js';
 import Property from '../../../../axon/js/Property.js';
-import Shape from '../../../../kite/js/Shape.js';
+import { Shape } from '../../../../kite/js/imports.js';
 import merge from '../../../../phet-core/js/merge.js';
 import ModelViewTransform2 from '../../../../phetcommon/js/view/ModelViewTransform2.js';
 import EyeDropperNode from '../../../../scenery-phet/js/EyeDropperNode.js';
