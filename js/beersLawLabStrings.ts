2266be56c85e2210dbebca92938a5be0f39eb131
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:39 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/beersLawLabStrings.ts
+++ js/beersLawLabStrings.ts
@@ -5,59 +5,98 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import beersLawLab from './beersLawLab.js';
 
 type StringsType = {
   'absorbance': string;
+  'absorbanceProperty': TReadOnlyProperty<string>;
   'beers-law-lab': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'cobaltIINitrate': string;
+  'cobaltIINitrateProperty': TReadOnlyProperty<string>;
   'cobaltChloride': string;
+  'cobaltChlorideProperty': TReadOnlyProperty<string>;
   'concentration': string;
+  'concentrationProperty': TReadOnlyProperty<string>;
   'copperSulfate': string;
+  'copperSulfateProperty': TReadOnlyProperty<string>;
   'drinkMix': string;
+  'drinkMixProperty': TReadOnlyProperty<string>;
   'evaporation': string;
+  'evaporationProperty': TReadOnlyProperty<string>;
   'preset': string;
+  'presetProperty': TReadOnlyProperty<string>;
   'lots': string;
+  'lotsProperty': TReadOnlyProperty<string>;
   'nickelIIChloride': string;
+  'nickelIIChlorideProperty': TReadOnlyProperty<string>;
   'none': string;
+  'noneProperty': TReadOnlyProperty<string>;
   'pattern': {
     '0label': string;
+    '0labelProperty': TReadOnlyProperty<string>;
     '0value': {
       '1units': string;
+      '1unitsProperty': TReadOnlyProperty<string>;
     };
     '0percent': string;
+    '0percentProperty': TReadOnlyProperty<string>;
     '0formula': {
       '1name': string;
+      '1nameProperty': TReadOnlyProperty<string>;
     };
     '0soluteAmount': string;
+    '0soluteAmountProperty': TReadOnlyProperty<string>;
   };
   'potassiumChromate': string;
+  'potassiumChromateProperty': TReadOnlyProperty<string>;
   'potassiumDichromate': string;
+  'potassiumDichromateProperty': TReadOnlyProperty<string>;
   'potassiumPermanganate': string;
+  'potassiumPermanganateProperty': TReadOnlyProperty<string>;
   'removeSolute': string;
+  'removeSoluteProperty': TReadOnlyProperty<string>;
   'saturated': string;
+  'saturatedProperty': TReadOnlyProperty<string>;
   'sodiumChloride': string;
+  'sodiumChlorideProperty': TReadOnlyProperty<string>;
   'solid': string;
+  'solidProperty': TReadOnlyProperty<string>;
   'solute': string;
+  'soluteProperty': TReadOnlyProperty<string>;
   'solution': string;
+  'solutionProperty': TReadOnlyProperty<string>;
   'screen': {
     'concentration': string;
+    'concentrationProperty': TReadOnlyProperty<string>;
     'beersLaw': string;
+    'beersLawProperty': TReadOnlyProperty<string>;
   };
   'transmittance': string;
+  'transmittanceProperty': TReadOnlyProperty<string>;
   'units': {
     'centimeters': string;
+    'centimetersProperty': TReadOnlyProperty<string>;
     'liters': string;
+    'litersProperty': TReadOnlyProperty<string>;
     'milliliters': string;
+    'millilitersProperty': TReadOnlyProperty<string>;
     'molesPerLiter': string;
+    'molesPerLiterProperty': TReadOnlyProperty<string>;
     'mM': string;
+    'mMProperty': TReadOnlyProperty<string>;
     'uM': string;
+    'uMProperty': TReadOnlyProperty<string>;
     'nm': string;
+    'nmProperty': TReadOnlyProperty<string>;
   };
   'variable': string;
+  'variableProperty': TReadOnlyProperty<string>;
   'wavelength': string;
+  'wavelengthProperty': TReadOnlyProperty<string>;
 };
 
 const beersLawLabStrings = getStringModule( 'BEERS_LAW_LAB' ) as StringsType;
