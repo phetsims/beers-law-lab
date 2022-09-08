88f0f429629f80590987664e625cc8cc88d01a00
pixelzoom <cmalley@pixelzoom.com>
2022-09-08 09:02:53 -0600

    replace ChemUtils.toSubscript with strings, https://github.com/phetsims/beers-law-lab/issues/287

--- js/common/BLLSymbols.js
+++ js/common/BLLSymbols.js
@@ -6,21 +6,20 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import ChemUtils from '../../../nitroglycerin/js/ChemUtils.js';
 import beersLawLab from '../beersLawLab.js';
 import BeersLawLabStrings from '../BeersLawLabStrings.js';
 
 const BLLSymbols = {
-  COBALT_II_NITRATE: ChemUtils.toSubscript( 'Co(NO3)2' ),
-  COBALT_CHLORIDE: ChemUtils.toSubscript( 'CoCl2' ),
-  COPPER_SULFATE: ChemUtils.toSubscript( 'CuSO4' ),
+  COBALT_II_NITRATE: 'Co(NO<sub>3</sub>)<sub>2</sub>',
+  COBALT_CHLORIDE: 'CoCl<sub>2</sub>',
+  COPPER_SULFATE: 'CuSO<sub>4</sub>',
   DRINK_MIX: BeersLawLabStrings.drinkMix,
-  NICKEL_II_CHLORIDE: ChemUtils.toSubscript( 'NiCl2' ),
-  POTASSIUM_CHROMATE: ChemUtils.toSubscript( 'K2CrO4' ),
-  POTASSIUM_DICHROMATE: ChemUtils.toSubscript( 'K2Cr2O7' ),
-  POTASSIUM_PERMANGANATE: ChemUtils.toSubscript( 'KMnO4' ),
-  SODIUM_CHLORIDE: ChemUtils.toSubscript( 'NaCl' ),
-  WATER: ChemUtils.toSubscript( 'H2O' )
+  NICKEL_II_CHLORIDE: 'NiCl<sub>2</sub>',
+  POTASSIUM_CHROMATE: 'K<sub>2</sub>CrO<sub>4</sub>',
+  POTASSIUM_DICHROMATE: 'K<sub>2</sub>Cr<sub>2</sub>O<sub>7</sub>',
+  POTASSIUM_PERMANGANATE: 'KMnO<sub>4</sub>',
+  SODIUM_CHLORIDE: 'NaCl',
+  WATER: 'H<sub>2</sub>O'
 };
 
 beersLawLab.register( 'BLLSymbols', BLLSymbols );
