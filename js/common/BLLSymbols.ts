fb83fe48d6f986dcea8d41f7b85fc8b00b245f0c
pixelzoom <cmalley@pixelzoom.com>
2022-09-12 14:56:34 -0600

    delete unused BLLSymbols.DRINK_MIX, https://github.com/phetsims/beers-law-lab/issues/288

--- js/common/BLLSymbols.ts
+++ js/common/BLLSymbols.ts
@@ -7,13 +7,11 @@
  */
 
 import beersLawLab from '../beersLawLab.js';
-import BeersLawLabStrings from '../BeersLawLabStrings.js';
 
 const BLLSymbols = {
   COBALT_II_NITRATE: 'Co(NO<sub>3</sub>)<sub>2</sub>',
   COBALT_CHLORIDE: 'CoCl<sub>2</sub>',
   COPPER_SULFATE: 'CuSO<sub>4</sub>',
-  DRINK_MIX: BeersLawLabStrings.drinkMix,
   NICKEL_II_CHLORIDE: 'NiCl<sub>2</sub>',
   POTASSIUM_CHROMATE: 'K<sub>2</sub>CrO<sub>4</sub>',
   POTASSIUM_DICHROMATE: 'K<sub>2</sub>Cr<sub>2</sub>O<sub>7</sub>',
