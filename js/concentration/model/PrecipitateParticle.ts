4460574a49e68f949f6c35ccddc2af539019d103
phet-dev <phetadmin@gmail.com>
2022-09-13 04:23:43 -0600

    update copyright dates from daily grunt work

--- js/concentration/model/PrecipitateParticle.ts
+++ js/concentration/model/PrecipitateParticle.ts
@@ -1,4 +1,4 @@
-// Copyright 2013-2021, University of Colorado Boulder
+// Copyright 2013-2022, University of Colorado Boulder
 
 /**
  * PrecipitateParticle is one particle that makes up the precipitate that forms on the bottom of the beaker.
