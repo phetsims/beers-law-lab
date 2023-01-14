17e5a9ec241e3df76143b030d265c98ced85fddc
phet-dev <phetadmin@gmail.com>
2023-01-14 04:06:10 -0700

    update copyright dates from daily grunt work

--- js/common/view/BeakerUnitsControl.ts
+++ js/common/view/BeakerUnitsControl.ts
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2022-2023, University of Colorado Boulder
 
 /**
  * BeakerUnitsControl is the Preferences dialog control for setting the units for the beaker.
--- js/common/view/ConcentrationMeterUnitsControl.ts
+++ js/common/view/ConcentrationMeterUnitsControl.ts
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2022-2023, University of Colorado Boulder
 
 /**
  * ConcentrationMeterUnitsControl is the Preferences dialog control for setting the units for the concentration meter.
