f730c14945219a0169f02cd19f9390ae2c73b214
phet-dev <phetadmin@gmail.com>
2023-02-01 03:43:56 -0700

    update copyright dates from daily grunt work

--- js/beerslaw/view/WavelengthPanel.ts
+++ js/beerslaw/view/WavelengthPanel.ts
@@ -1,4 +1,4 @@
-// Copyright 2013-2022, University of Colorado Boulder
+// Copyright 2013-2023, University of Colorado Boulder
 
 /**
  * WavelengthPanel is the panel that contains controls related to wavelength of the light.
