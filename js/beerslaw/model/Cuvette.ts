a8b17e33b5fd1ebd1c695567268da7898be08c35
phet-dev <phetadmin@gmail.com>
2023-03-10 04:11:50 -0700

    update copyright dates from daily grunt work

--- js/beerslaw/model/Cuvette.ts
+++ js/beerslaw/model/Cuvette.ts
@@ -1,4 +1,4 @@
-// Copyright 2013-2022, University of Colorado Boulder
+// Copyright 2013-2023, University of Colorado Boulder
 
 /**
  * Cuvette is a simple model of a cuvette. A cuvette is a small tube of circular or square cross section,
