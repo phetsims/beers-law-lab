acb026145fe6b9d8a16a4c79da0d0aefd767276e
pixelzoom <cmalley@pixelzoom.com>
2023-05-15 16:27:37 -0600

    delete unintentional featuring of 3 solutes, https://github.com/phetsims/beers-law-lab/issues/331

--- js/beers-law-lab-phet-io-overrides.js
+++ js/beers-law-lab-phet-io-overrides.js
@@ -1,13 +1,2 @@
 /* eslint-disable */
-window.phet.preloads.phetio.phetioElementsOverrides =
-  {
-    "beersLawLab.global.model.solutes.potassiumDichromate": {
-      "phetioFeatured": true
-    },
-    "beersLawLab.global.model.solutes.potassiumPermanganate": {
-      "phetioFeatured": true
-    },
-    "beersLawLab.global.model.solutes.sodiumChloride": {
-      "phetioFeatured": true
-    }
-  };
+window.phet.preloads.phetio.phetioElementsOverrides = {};
