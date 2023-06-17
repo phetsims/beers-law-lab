4855afc3af53890f6423768cebe1b531fd8e6297
phet-dev <phetadmin@gmail.com>
2023-06-17 03:28:22 -0600

    update copyright dates from daily grunt work

--- js/concentration/view/SolutionVolumeNode.ts
+++ js/concentration/view/SolutionVolumeNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2022-2023, University of Colorado Boulder
 
 /**
  * SolutionVolumeNode displays the volume of the solution, in L. See https://github.com/phetsims/beers-law-lab/issues/161
