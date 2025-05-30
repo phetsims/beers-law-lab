da28ac31a084b85e3a05621091abc19a39755bf4
phet-dev <phetadmin@gmail.com>
2025-05-30 03:14:41 -0600

    update copyright dates from daily grunt work

--- js/concentration/model/ShakerParticles.ts
+++ js/concentration/model/ShakerParticles.ts
@@ -1,4 +1,4 @@
-// Copyright 2013-2024, University of Colorado Boulder
+// Copyright 2013-2025, University of Colorado Boulder
 
 /**
  * ShakerParticles manages the lifetime of solute particles, from creation when they exit the shaker,
