b559fa513ee981d67bf4a0db01e213a710f932a2
phet-dev <phetadmin@gmail.com>
2025-07-01 03:15:11 -0600

    update copyright dates from daily grunt work

--- js/beerslaw/model/ATDetector.ts
+++ js/beerslaw/model/ATDetector.ts
@@ -1,4 +1,4 @@
-// Copyright 2013-2024, University of Colorado Boulder
+// Copyright 2013-2025, University of Colorado Boulder
 
 /**
  * ATDetector is the detector for absorbance (A) and percent transmittance (%T) of light passing through
