c8f8f180a175bef9d590eda706f3a9add493a2f3
phet-dev <phetadmin@gmail.com>
2025-07-30 03:14:23 -0600

    update copyright dates from daily grunt work

--- js/beerslaw/model/DetectorMode.ts
+++ js/beerslaw/model/DetectorMode.ts
@@ -1,4 +1,4 @@
-// Copyright 2021-2022, University of Colorado Boulder
+// Copyright 2021-2025, University of Colorado Boulder
 
 /**
  * DetectorMode is an enumeration of the modes for the detector that measures transmittance or absorbance.
