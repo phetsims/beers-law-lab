5b751d7e190b5ae1d2d294fb380fbc3f6effdb22
phet-dev <phetadmin@gmail.com>
2025-09-04 03:42:09 -0600

    update copyright dates from daily grunt work

--- js/concentration/model/ConcentrationMeter.ts
+++ js/concentration/model/ConcentrationMeter.ts
@@ -1,4 +1,4 @@
-// Copyright 2013-2023, University of Colorado Boulder
+// Copyright 2013-2025, University of Colorado Boulder
 
 /**
  * ConcentrationMeter is the model of the concentration meter in the 'Concentration' screen.
