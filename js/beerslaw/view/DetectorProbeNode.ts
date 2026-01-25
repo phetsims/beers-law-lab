c242979e592f4172adedc98851e1d883ff29e5ad
phet-dev <phetadmin@gmail.com>
2026-01-25 03:42:32 -0700

    update copyright dates from daily grunt work

--- js/beerslaw/view/DetectorProbeNode.ts
+++ js/beerslaw/view/DetectorProbeNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2025, University of Colorado Boulder
+// Copyright 2025-2026, University of Colorado Boulder
 
 /**
  * DetectorProbeNode is the draggable probe for measuring transmittance and absorbance.
